b1f0fe76de63c40582fd03bd05ffea151da13d6f
Yassin Nasrallah <yassinnasrallah427@gmail.com>
2026-06-18 02:19:30 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './styles/Main.css'
+import './App.css';
+import Main from './pages/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import DarkMode from './DarkMode'
+
+const Navigation = () => {
+  return (
+    <div>
+        <nav className="nav-container">
+            <div className="logo">
+                <h1>MUSIC</h1>
+            </div>
+           
+        </nav>
+    </div>
+  )
+}
+
+export default Navigation
